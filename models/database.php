9b334b06a7fc8b6bb342ab24b61ce768d5ba4018
Noah VV <vvnoah@hotmail.com>
2022-10-08 09:59:32 +0100

    closes #3 Established connection to database.

--- models/database.php
+++ models/database.php
@@ -3,4 +3,10 @@
     $username = 'root';
     // $password = 'password';
     
-    
+    try{
+        $database = new PDO($dsn, $username);
+    } catch (PDOException $ex) {
+        $error = 'Database error: ';
+        $error .= $ex->getMessage();
+        include(views/error.php);
+    }

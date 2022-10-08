a44a5312885bd07e42f3501aba494f7376c97cb8
Noah VV <vvnoah@hotmail.com>
2022-10-08 13:37:12 +0100

    closes #9 Added course list view

--- views/list-courses.php
+++ views/list-courses.php
@@ -1,12 +1,43 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        // put your code here
-        ?>
-    </body>
-</html>
+<?php include('header.php') ?>
+
+<?php if($courses){ ?>
+    <section id="list" class="list">
+        <header class="list__row list__header">
+            <h1>Courses</h1>
+        </header>
+        <?php foreach($courses as $course){ ?>
+            <div class="list__row">
+                <div class="list__item">
+                    <p><?php echo $course['course_name'] ?></p>
+                </div>
+                <div class=""list__remove_item>
+                    <form action="." method="post">
+                        <input type="hidden" name="action" value="delete_course">
+                        <input type="hidden" name="course_id" value="<?php $course['course_id'] ?>">
+                    </form>
+                </div>
+            </div>
+        <?php } ?>
+    </section>
+<?php } else { ?>
+    <p>No courses exist yet.</p>
+<?php } ?>
+   
+<section id="add" class="add">
+    <h2>Add course</h2>
+    <form action="." method="post" id="add__form" class="add__form">
+        <input type="hidden" name="action" value="add_course">
+        <div class="add__inputs">
+            <label>name:</label>
+            <input type="text" name="course_name" maxlength="50" placeholder="name" required>
+        </div>
+        <div class="add__add_item">
+            <button class="add_button bold">Add</button>
+        </div>
+    </form>
+</section>
+    
+<br>
+<p><a href=".">View &amp; add assignments</a></p>
+    
+<?php include('footer.php') ?>
